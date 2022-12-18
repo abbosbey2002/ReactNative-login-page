5fae945f228ef444f7de6a4062013c474445aba9
abbosbey <abbosbeyqudratov@gmail.com>
2022-12-18 18:37:13 +0500

    my first react native app login page

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Platform, PlatformColor } from 'react-native';
+import { TextInput } from 'react-native-web';
+import './style.css'
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{Platform.OS}</Text>
+    <View style={styles.box}>
+      <Text style={styles.login}>Login</Text>
+      <label>Email</label>
+      <TextInput style={styles.input} />
+      <label>Password</label>
+      <TextInput style={styles.input} />
+      <Text style={styles.forget}>Forget password</Text>
+      <Button color="#28aeba" title='submit' />
+      <Text style={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente laboriosam</Text>
+    </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#28aeba',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  login:{
+    fontSize: "20px",
+    fontWeight: "700",
+    textAlign: "center"
+  },
+  Text:{
+    color: "green",
+    fontSize: "15px",
+    backgroundColor: "white",
+    padding: "10px"
+  },
+  box:{
+    width: '20rem',
+    backgroundColor: "white",
+    padding: "20px",
+    borderRadius: "5px"
+  },
+  input:{
+    backgroundColor: "white",
+    borderColor: 'gainsboro',
+    borderWidth: "2px",
+    marginBottom: "10px",
+    padding: "4px"
+  },
+  desc:{
+    letterSpacing: '1px',
+    fontSize: '14px',
+    marginTop: "10px",
+    lineHeight: "1.2"
+  },
+  forget:{
+    margin: '5px',
+    color: "#551a8b",
+    
+  }
+  
 });
